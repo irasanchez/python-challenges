4910febc8fa5f430f7f98b1daa1efce94cb23bb4
Ira <ira.d.sanchez@gmail.com>
2020-01-19 21:31:41 -0700

    Add 2 Completed Challenges

---
+++ challenges.py
@@ -0,0 +1,30 @@
+# This program reads two numbers and prints their sum:
+# get input and convert to integer
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+
+# Can you change it so it can read and sum three numbers?
+
+# Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+# Every number is given on a separate line.
+# a = 1/2bh
+
+# Read the numbers b and h like this:
+b2 = int(input())
+h = int(input())
+print((h * b2) / 2)
+
+# Print the result with print()
+
+# Write a program that greets the user by printing the word "Hello", a comma, the name of the user
+# and an exclamation mark after it. See the examples below.
+# Warning. Your program's output should strictly match the desired one, character by character.
+# There shouldn't be any space between the name and the exclamation mark. You can use + operator to concatenate
+# two strings. See the lesson for details.
+
+# Read a string:
+# a2 = input()
+# Print a string:
+# print(a2)
